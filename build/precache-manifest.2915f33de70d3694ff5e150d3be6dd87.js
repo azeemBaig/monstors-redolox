d4f25f3778b6c266caab0eb37fb387c29c74e071
azeemBaig <azeem.baig.insomniac@gmail.com>
2020-03-27 23:34:49 +0530

    first

---
+++ build/precache-manifest.2915f33de70d3694ff5e150d3be6dd87.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ef3a56b822261444adcdde1276a8a17",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "138520cba4eec694aa99",
+    "url": "/monsters-rolodex/static/css/main.db18944d.chunk.css"
+  },
+  {
+    "revision": "8a6eda421bc5765c55c2",
+    "url": "/monsters-rolodex/static/js/2.bc65f737.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.bc65f737.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "138520cba4eec694aa99",
+    "url": "/monsters-rolodex/static/js/main.70eb9a00.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import { CardList } from './components/card-list/card-list.component'
+import './App.css';
+import { SearchBox } from './components/search-box/search-box.component';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      monsters: [ ],
+      searchFeild : ''
+    }
+  }
+
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res => res.json())
+    .then(users => this.setState({monsters : users}))
+  }
+
+  render() {
+    const { monsters, searchFeild } = this.state
+    const filteredMonsters = monsters.filter(monster =>
+      monster.name.toLowerCase().includes(searchFeild.toLowerCase()))
+    return (
+      <div className="App">
+        <h1>Monsters Rolodex</h1>
+        <SearchBox
+          placeholder='search monster'
+          handleChange={e=>this.setState({searchFeild: e.target.value})}
+        />
+        <CardList monsters={filteredMonsters}/>
+      </div>
+    )
+  }
+}
+
+export default App;
